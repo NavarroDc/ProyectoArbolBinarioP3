01bbc053e78df52800e02f21457c21a924ba14c7
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2025-07-30 17:35:27 -0600

    creados clase Tarjeta y Jframe Insercion

---
+++ ED DavidNavarroC.-Proyecto3/src/ed/davidnavarroc/proyecto3/Tarjeta.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ed.davidnavarroc.proyecto3;
+
+public class Tarjeta {
+    private int id;
+    private String descripcion;
+    private String categoria;
+
+    public Tarjeta(int id, String descripcion, String categoria) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
