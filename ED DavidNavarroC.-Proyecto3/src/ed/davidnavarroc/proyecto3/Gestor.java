06872391a88e0a59a90d240a8648bde66f059d72
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2025-08-06 11:24:41 -0600

    haciendo insercion

--- ED DavidNavarroC.-Proyecto3/src/ed/davidnavarroc/proyecto3/Gestor.java
+++ ED DavidNavarroC.-Proyecto3/src/ed/davidnavarroc/proyecto3/Gestor.java
@@ -2,4 +2,56 @@ package ed.davidnavarroc.proyecto3;
 
 public class Gestor {
     
+    private Nodo nodoRaiz;
+
+    public Gestor() {
+        this.nodoRaiz = null;
+    }
+
+    public Gestor(Nodo nodoRaiz) {
+        this.nodoRaiz = null;
+    }
+    
+    public boolean tarjetaInsertada(Tarjeta tarjeta){
+        if(buscarTarjetaID(tarjeta.getId()) != null){
+            return false; //Significa que ya existe el ID
+        }
+        
+        nodoRaiz = insertarDatos(nodoRaiz, tarjeta);
+        return true;
+    }
+    
+    public Nodo insertarDatos(Nodo nodoActual, Tarjeta tarjeta){
+        if(nodoActual == null){
+        return new Nodo(tarjeta);
+        }
+        
+        if(tarjeta.getId() < nodoActual.tarjeta.getId()){
+            nodoActual.nodoIzquierdo = insertarDatos(nodoActual.nodoIzquierdo, tarjeta);
+        }else{
+            nodoActual.nodoDerecho = insertarDatos(nodoActual.nodoDerecho, tarjeta);
+        }
+        return nodoActual;
+    }
+    
+    public Tarjeta buscarTarjetaID(int id){
+        
+        return buscarDatosTarjeta(nodoRaiz, id);
+    }
+    
+    private Tarjeta buscarDatosTarjeta(Nodo nodoActual, int id){
+        if(nodoActual==null){
+            return null;
+        }
+        
+        if(id == nodoActual.tarjeta.getId()){
+            return nodoActual.tarjeta;
+        }
+        
+        if(id < nodoActual.tarjeta.getId()){
+            return buscarDatosTarjeta(nodoActual.nodoIzquierdo, id);
+        }else{
+            return buscarDatosTarjeta(nodoActual.nodoDerecho, id);
+        }
+    }
 }
--- ED DavidNavarroC.-Proyecto3/src/ed/davidnavarroc/proyecto3/Nodo.java
+++ ED DavidNavarroC.-Proyecto3/src/ed/davidnavarroc/proyecto3/Nodo.java
@@ -1,42 +1,23 @@
 package ed.davidnavarroc.proyecto3;
 
 public class Nodo {
-    int id; 
-    String descripcion;
-    String categoria;
+   
+    Tarjeta tarjeta;
     Nodo nodoIzquierdo;
     Nodo nodoDerecho;
 
-    public Nodo(int id, String descripcion, String categoria, Nodo nodoIzquierdo, Nodo nodoDerecho) {
-        this.id = id;
-        this.descripcion = descripcion;
-        this.categoria = categoria;
+    public Nodo(Tarjeta tarjeta) {
+        this.tarjeta = tarjeta;
         this.nodoIzquierdo = null;
         this.nodoDerecho = null;
     }
 
-    public int getId() {
-        return id;
+    public Tarjeta getTarjeta() {
+        return tarjeta;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
+    public void setTarjeta(Tarjeta tarjeta) {
+        this.tarjeta = tarjeta;
     }
 
     public Nodo getNodoIzquierdo() {
@@ -54,4 +35,6 @@ public class Nodo {
     public void setNodoDerecho(Nodo nodoDerecho) {
         this.nodoDerecho = nodoDerecho;
     }
+
+    
 }
